23b2338012a8faee02b727b175670f018865ce45
Tom Vogels <tom@harrys.com>
2018-03-29 15:13:10 -0400

    wip

--- python/etl/text.py
+++ python/etl/text.py
@@ -5,6 +5,8 @@ Should not import Arthur modules (so that etl.errors remains widely importable)
 
 import textwrap
 
+from tabulate import tabulate
+
 
 def join_with_quotes(names):
     """
@@ -37,8 +39,6 @@ def join_column_list(columns):
 class ColumnWrapper(textwrap.TextWrapper):
     """
     Unlike the TextWrapper, we don't care about words and just treat the entire column as a chunk.
-
-    If the column is "too long", then we force a "word break" with whitespace.
     """
 
     def _split(self, text):
@@ -62,33 +62,32 @@ def format_lines(value_rows, header_row=None, has_header=False, max_column_width
     """
     Format a list of rows which each have a list of values, optionally with a header.
 
-    We process whitespace by expanding tabs and then replacing all whitespace but actual spaces.
-    If any column is longer than the max_column_width, a placeholder will be inserted after cutting the value.
-
     >>> print(format_lines([["aa", "b", "ccc"], ["a", "bb", "c"]]))
-    col #1 | col #2 | col #3
-    -------+--------+-------
-    aa     | b      | ccc
-    a      | bb     | c
+     col #1   | col #2   | col #3
+    ----------+----------+----------
+     aa       | b        | ccc
+     a        | bb       | c
     (2 rows)
     >>> print(format_lines([["name", "breed"], ["monty", "spaniel"], ["cody", "poodle"], ["cooper", "shepherd"]],
     ...                    has_header=True))
-    name   | breed
-    -------+---------
-    monty  | spaniel
-    cody   | poodle
-    cooper | shepherd
+     name   | breed
+    --------+----------
+     monty  | spaniel
+     cody   | poodle
+     cooper | shepherd
     (3 rows)
     >>> print(format_lines([["windy"]], header_row=["weather"]))
-    weather
-    -------
-    windy
+     weather
+    -----------
+     windy
     (1 row)
+    >>> print(format_lines([["some long line", "second column"]], max_column_width=6))
+     col #1   | col #2
+    ----------+----------
+     some     | second
+     long     | column
+     line     |
     >>> print(format_lines([]))
-    (0 rows)
-    >>> print(format_lines([], header_row=["nothing"]))
-    nothing
-    -------
     (0 rows)
     >>> format_lines([["a", "b"], ["c"]])
     Traceback (most recent call last):
@@ -96,35 +95,29 @@ def format_lines(value_rows, header_row=None, has_header=False, max_column_width
     """
     if header_row is not None and has_header is True:
         raise ValueError("cannot have separate header row and mark first row as header")
+
     # Make sure that we are working with a list of lists of strings (and not generators and such).
-    wrapper = ColumnWrapper(width=max_column_width, max_lines=1, placeholder="...",
+    wrapper = ColumnWrapper(width=max_column_width,
                             expand_tabs=True, replace_whitespace=True, drop_whitespace=False)
     matrix = [[wrapper.fill(str(column)) for column in row] for row in value_rows]
-    # Add header row as needed, have number of columns depend on header
-    if header_row is not None:
-        n_columns = len(header_row)
-        matrix.insert(0, [str(column) for column in header_row])
-    elif has_header:
-        n_columns = len(matrix[0])
-    else:
-        n_columns = len(matrix[0]) if len(matrix) > 0 else 0
-        matrix.insert(0, ["col #{:d}".format(i + 1) for i in range(n_columns)])
-    assert len(matrix) > 0
+
+    n_columns = len(matrix[0]) if len(matrix) > 0 else 0
     for i, row in enumerate(matrix):
         if len(row) != n_columns:
             raise ValueError("unexpected row length: got {:d}, expected {:d}".format(len(row), n_columns))
-    row_count = "({:d} {})".format(len(matrix) - 1, "row" if len(matrix) == 2 else "rows")
-    # Determine column widths, add separator between header and body as dashed line
-    column_width = [max(len(row[i]) for row in matrix) for i in range(n_columns)]
-    if max_column_width is not None:
-        column_width = [min(actual_width, max_column_width) for actual_width in column_width]
-    matrix.insert(1, ["-" * width for width in column_width])
-    # Now rewrite the matrix values to fill the columns
-    if header_row or len(matrix) > 2:
-        matrix = [["{:{}s}".format(row[i], column_width[i]) for i in range(n_columns)] for row in matrix]
-        rows = [' | '.join(row).rstrip() for row in matrix]
-        rows[1] = rows[1].replace(' | ', '-+-')
-        rows.append(row_count)
+
+    if header_row:
+        n_rows = len(matrix)
+        headers = header_row
+    elif has_header:
+        n_rows = len(matrix) - 1
+        headers = "firstrow"
     else:
-        rows = [row_count]  # without data, don't even try to print the column headers, like "col #1".
-    return '\n'.join(rows)
+        n_rows = len(matrix)
+        headers = ["col #{:d}".format(i + 1) for i in range(n_columns)]
+    row_count = "({:d} {})".format(n_rows, "row" if n_rows == 1 else "rows")
+    if n_rows:
+        lines = tabulate(matrix, headers=headers, tablefmt="presto")
+        return lines + '\n' + row_count
+    else:
+        return row_count
